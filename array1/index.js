4b62e850b89e1818a26bbaf46703237dbb77d223
Radiant <you@example.com>
2020-09-10 23:03:55 +0700

    frist commit

---
+++ array1/index.js
@@ -0,0 +1,19 @@
+var listdesabantul = [
+    'Argodadi',
+    'Argorejo',
+    'Argosari', 
+    [
+        'Bangunjiwo',
+        'Banguntapan',
+        'Bantul',
+        'Banturetno',
+        'Bawuran',
+        [
+            'Canden',
+            'Caturharjo',
+            'Cetan Srigading'
+        ]
+        
+    ]
+]
+console.log(listdesabantul[3][5][0]);
